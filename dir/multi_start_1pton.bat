ef179d55ce5c91be429ce119616ad6b7647df450
llll3 <90038489+llll3@users.noreply.github.com>
2024-07-12 03:03:02 +0300

    Update multi_start_1pton.bat

--- dir/multi_start_1pton.bat
+++ dir/multi_start_1pton.bat
@@ -3,13 +3,13 @@
 setlocal
 
 set count=0
-cd..
-for /f %%a in ('findstr /R /N "^" api\api_key_1pton.txt ^| findstr /C:"^$"') do set count=%%a
+cd ..
+for /f %%a in ('findstr /R /N "^" api\api_key_shutle.txt ^| findstr /C:"^$"') do set count=%%a
 
 setlocal enabledelayedexpansion
-for /f "tokens=1 delims=-" %%a in (api\api_key_1pton.txt) do (
-  set /a count-=1  
-  start "" "dir\console.lnk" /c dir\main.exe 1 1 %%a
+for /f "tokens=1 delims=-" %%a in (api\api_key_shutle.txt) do (
+  set /a count-=1
+  start "" "dir\console.lnk" /c dir\main.exe 2 1 %%a
   timeout /t 1 >nul  
   if !count! equ 0 goto endloop
 )
